c97baddc9eb679f556a31b83615b05db2150a31c
nma85@mail.aub.edu <naderalawar@gmail.com>
2018-11-12 08:48:34 +0200

    added notifications service

---
+++ ChatService.Common/Notifications/NotificationsServiceSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatService.Notifications
+{
+    public class NotificationsServiceSettings
+    {
+        public string BaseUri { get; set; }
+    }
+}
