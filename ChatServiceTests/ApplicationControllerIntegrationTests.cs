d7155e9aba5ac4da3643d938f6b9bf1c815d7949
nma85@mail.aub.edu <naderalawar@gmail.com>
2018-12-17 13:40:22 +0200

    added tests for the application controller

---
+++ ChatServiceTests/ApplicationControllerIntegrationTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using ChatService;
+using ChatService.Client;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChatServiceTests
+{
+    [TestClass]
+    [TestCategory("Integration")]
+    public class ApplicationControllerIntegrationTests
+    {
+        private HttpClient httpClient;
+        private TestServer server;
+        private ChatServiceClient chatServiceClient;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            server = new TestServer(WebServer.CreateWebHostBuilder());
+            httpClient = server.CreateClient();
+            chatServiceClient = new ChatServiceClient(httpClient);
+        }
+
+        [TestMethod]
+        public async Task GetVersionTest()
+        {
+            var response = await chatServiceClient.GetVersion();
+            Assert.IsNotNull(response);
+        }
+
+        [TestMethod]
+        public async Task GetEnvironmentTest()
+        {
+            var response = await chatServiceClient.GetEnvironment();
+            Assert.IsNotNull(response);
+        }
+    }
+}
